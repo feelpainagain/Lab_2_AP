e8c92a7b685d7527849acf803a20601522d6e6c8
feelpainagain <132294998+feelpainagain@users.noreply.github.com>
2023-12-18 17:31:52 +0400

    final+funcs
    added some funcs in main

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
 import os
 import pandas as pd
 
-columns = ["Absolute path", "Relative path", "Class"]
-data = []
-rel_path1 = r'dataset\tiger'
-rel_path2 = r'dataset\leopard'
+def save_as_csv(to_save: list[list[str]], columns: list[str], relpath: str) -> None:
+    """func that saves data in csv file 
 
-name_list1 = os.listdir(rel_path1)
-name_list2 = os.listdir(rel_path2)
+    Args:
+        to_save (list[list[str]]): string table (matrix) that is needed to be saved formated 
 
-for name in name_list1:
-    data.append([os.path.abspath(rel_path1 + '\\' + name), f'{rel_path1}\\{name}', "tiger"])
-for name in name_list2:
-    data.append([os.path.abspath(rel_path2 + '\\' + name), f'{rel_path2}\\{name}', "leopard"])
+        columns (list[str]): name of columns
+        relpath (str): relative path for data 
+    """
+    df = pd.DataFrame(to_save, columns=columns)
+    df.to_csv(relpath, sep=";")
+    print(f'Successfully saved in {relpath}')
 
-df = pd.DataFrame(data, columns=columns)
-df.to_csv('annotation.csv', sep=";")
+def scan_dataset(folder_paths: list[str]) -> list[list[str]]:
+    """this func scans given dataset and converts it for saving as csv
+
+    Args:
+        folder_paths (list[str]): paths to dataset
+
+    Returns:
+        list[list[str]]: data is a matrix of strings
+    """
+    result = []
+    for folder in folder_paths:
+        item_class = folder.split('\\')[-1]
+        for name in os.listdir(folder):
+            result.append([os.path.abspath(folder + '\\' + name), f'{folder}\\{name}', item_class])
+    return result
+
+if __name__ == "__main__":
+    rel_path1 = r'dataset\tiger'
+    rel_path2 = r'dataset\leopard'
+
+    columns = ["Absolute path", "Relative path", "Class"]
+    data = scan_dataset([rel_path1, rel_path2])
+    save_as_csv(data, columns, 'annotation.csv')
